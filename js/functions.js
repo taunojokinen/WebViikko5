8d5c881766e9a747156ec280b8a178884c24a7a7
taunojokinen <128710496+taunojokinen@users.noreply.github.com>
2024-02-06 13:34:30 +0200

    toimii

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+const dice = document.getElementById('myImage')
+const randNum = (min,max) => {
+    return Math.floor(Math.random() *max) + min
+}
+
+dice.addEventListener('click',() => {
+    dice.src="./img/" +randNum(1,6) +".png" 
+})
+
+
+
